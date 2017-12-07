c4b6b4603ea87d1eff073dbe3af5029a2b8850f6
Cheng2 <rayxx1234@gmail.com>
2017-12-07 13:10:17 +0800

    7Dec Applet update

--- CSIE/Fitbit_web.bat
+++ CSIE/Fitbit_web.bat
@@ -1,2 +1,2 @@
-D:
-D:%\Program files\Anaconda3%\python -m http.server 8000
+cd C:\Users\csie\Documents\GitHub\Csie-web
+C:\Users\csie\AppData\Local\Programs\Python\Python36-32\python -m http.server 8000
